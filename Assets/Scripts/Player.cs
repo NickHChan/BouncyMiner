bdd4598f37f2ebaa6cf0b0d023991aea05ddf38d
Nick Chan <nickharuchan@gmail.com>
2024-12-10 11:00:10 -0500

    Added method to change player modes with button press

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,12 +15,29 @@ public class Player : MonoBehaviour
     void Start()
     {
      rb = GetComponent<Rigidbody2D>();   
+     Debug.Log("To Changes Modes use 1:Mining 2: Attacking 3: Defending");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangePlayerMode();
+    }
+
+	private void ChangePlayerMode()
+	{
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayerMode = PlayerModes.Mining;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayerMode = PlayerModes.Attacking;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayerMode = PlayerModes.Defending;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
