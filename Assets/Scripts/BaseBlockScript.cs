1fb9acdf1e45e78851a3c15763aad5129caaf724
Nick Chan <nickharuchan@gmail.com>
2025-01-06 02:22:09 -0500

    Created method to spawn a powerup on baseBlock destruction

--- Assets/Scripts/BaseBlockScript.cs
+++ Assets/Scripts/BaseBlockScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BaseBlockScript : MonoBehaviour
 {
     [SerializeField] private int _blockHealth = 1;
+    [SerializeField] private GameObject powerUpPrefab;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -24,11 +25,17 @@ public class BaseBlockScript : MonoBehaviour
         if (_blockHealth <= 0)
         {
             Destroy(gameObject);
+            CreatePowerUp();
         }
     }
 
     public void AddBlockHealth(int health)
     {
         _blockHealth += health;
+    }
+
+    private void CreatePowerUp()
+    {
+        Instantiate(powerUpPrefab, transform.position, Quaternion.identity);
     }
 }
