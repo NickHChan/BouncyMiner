f93d9ff510f9335e669b8c66542a89b8a6643757
Nick Chan <nickharuchan@gmail.com>
2025-04-01 02:10:33 -0400

    Added methods in settings scripts to check PlayerPref for NeedTutorial key, and attached save and cancel button methods to save the value of the key

--- Assets/Scripts/MainMenuScripts/SettingsScripts.cs
+++ Assets/Scripts/MainMenuScripts/SettingsScripts.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScripts : MonoBehaviour
 {
+    [SerializeField] private Toggle tutorialToggle;
     private void Awake()
     {
         gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        LoadTutorialToggle();
     }
 
     public void CancelButton()
@@ -15,5 +23,21 @@ public class SettingsScripts : MonoBehaviour
     public void SaveButton()
     {
         gameObject.SetActive(false);
+    }
+
+    private void LoadTutorialToggle()
+    {
+        if (!PlayerPrefs.HasKey("NeedTutorial")) return;
+        tutorialToggle.isOn = PlayerPrefs.GetInt("NeedTutorial") == 1;
+    }
+
+    public void SaveTutorialToggle()
+    {
+        PlayerPrefs.SetInt("NeedTutorial", tutorialToggle.isOn ? 1 : 0);
+    }
+
+    public void CancelTutorialToggle()
+    {
+        PlayerPrefs.SetInt("NeedTutorial", tutorialToggle.isOn ? 1 : 0);
     }
 }
